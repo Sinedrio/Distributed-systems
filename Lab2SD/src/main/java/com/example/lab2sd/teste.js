c76feff25bc2b8f007e4223dc0d81090c40dc50f
João Lucas Rocha <89816696+Sinedrio@users.noreply.github.com>
2024-04-19 19:07:18 -0300

    Delete Lab2SD directory

--- Lab2SD/src/main/java/com/example/lab2sd/teste.js
+++
@@ -1,7 +0,0 @@
-// script.js
-
-function calcularAreaRetangulo(altura, largura) {
-    return altura * largura;
-}
-
-console.log("A área de um retângulo de altura 5 e largura 10 é: " + calcularAreaRetangulo(5, 10));
